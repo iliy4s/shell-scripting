6fd4a22913cd5b7a7cba95388feecefce40c6c31
iliy4s <iliyas.clou6@gmail.com>
2026-02-13 23:41:43 +0530

    Rename function to list collaborators instead of issues

--- list-issues.sh
+++ list-issues.sh
@@ -2,32 +2,32 @@
 ################################################
 # author : iliyas
 # date : 8-2-26
-# content : bash script to list issues in a repo 
+# content : bash script to list collabrator of repo 
 ################################################
 api_url="https://api.github.com"
-username=$username
-token=$token 
+username=${username}
+token=${token} 
 
 repo_owner=$1
 repo_name=$2
 function github_api_get {
   local endpoint="$1"
-  local url=${api_utl}/${endpoint}
+  local url=${api_url}/${endpoint}
 
-  curl -s -u "${username}:${token}" "url"
+  curl -s -u "${username}:${token}" "$url"
   #collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
 }
-function get_issues {
+function get_collabrator {
   local endpoint="repos/${repo_owner}/${repo_name}/issues"
-  issues="$(github_api_get "$endpoint")"
+  collabrator="$(github_api_get "$endpoint")"
   #| jq -r .[] | select(.permissions.pull == true) | .login
-  if [[ -z "$issues" ]]; then
+  if [[ -z "$collabrator" ]]; then
     echo "no issues in ${repo_name}."
   else 
     echo "issues in ${repo_name} are:"
-    echo "${issues}"
+    echo "${collabrator}"
   fi
 }
 #main script
 echo "listing issues in ${repo_owner}/${repo_name}"
-get_issues
+get_collabrator
